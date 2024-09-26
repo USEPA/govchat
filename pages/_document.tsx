50719843c07998a8779b062a20bad024017ce113
Andy Chase <chase.andrew@epa.gov>
2024-09-26 13:37:33 -0700

    Update analytics tracking

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,8 @@
 import { DocumentProps, Head, Html, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 
 import i18nextConfig from '../next-i18next.config';
+import { GoogleTagManager } from '@next/third-parties/google'
+
 
 type Props = DocumentProps & {
   // add custom document props
@@ -12,29 +13,12 @@ export default function Document(props: Props) {
     props.__NEXT_DATA__.locale ?? i18nextConfig.i18n.defaultLocale;
   return (
     <Html lang={currentLocale}>
+      <GoogleTagManager gtmId="GTM-W55ZK7" />
       <Head>
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-title" content="Gov Chat"></meta>
-        <Script
-          id="google-tag-manager"
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-W55ZK7');`,
-          }}
-        />
       </Head>
       <body>
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-W55ZK7"
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          ></iframe>
-        </noscript>
         <Main />
         <NextScript />
       </body>
