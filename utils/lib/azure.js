9aaf6b078c8883c7704e6442e106b190a41fa60e
Andy Chase <theandychase@gmail.com>
2024-10-01 20:27:37 -0700

    Changes to make api work and also better errors for no managed identity

--- utils/lib/azure.js
+++ utils/lib/azure.js
@@ -1,14 +1,17 @@
 // lib/azure.js
 
-import { DefaultAzureCredential } from "@azure/identity";
 //import { getCache, setCache } from "./cache";
 
 
 
 export async function getAuthToken() {
+  if (process.env.AZURE_USE_MANAGED_IDENTITY != "true") {
+    return "";
+  }
+  const { ManagedIdentityCredential } = require("@azure/identity");
   let cachedToken = process.env.AUTH_TOKEN ? JSON.parse(process.env.AUTH_TOKEN) : '';
   if (!cachedToken || cachedToken.expiresOnTimestamp < Date.now()) {
-    let cachedCredential = new DefaultAzureCredential();
+    let cachedCredential = new ManagedIdentityCredential();
     cachedToken = await cachedCredential.getToken("https://cognitiveservices.azure.com/.default");
     process.env.AUTH_TOKEN = JSON.stringify(cachedToken);
     return cachedToken;
@@ -16,4 +19,3 @@ export async function getAuthToken() {
   // console.log("cachedToken from memory", cachedToken);
   return process.env.AUTH_TOKEN ? JSON.parse(process.env.AUTH_TOKEN) : '';;
 }
- 
