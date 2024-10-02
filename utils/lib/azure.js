2b73c3336a2cb0d311a48a251d19b13d92c781c5
Andy Chase <theandychase@gmail.com>
2024-10-01 21:23:23 -0700

    Revert "Changes to make api work and also better errors for no managed identity"
    This reverts commit 9aaf6b078c8883c7704e6442e106b190a41fa60e.

--- utils/lib/azure.js
+++ utils/lib/azure.js
@@ -1,17 +1,14 @@
 // lib/azure.js
 
+import { DefaultAzureCredential } from "@azure/identity";
 //import { getCache, setCache } from "./cache";
 
 
 
 export async function getAuthToken() {
-  if (process.env.AZURE_USE_MANAGED_IDENTITY != "true") {
-    return "";
-  }
-  const { ManagedIdentityCredential } = require("@azure/identity");
   let cachedToken = process.env.AUTH_TOKEN ? JSON.parse(process.env.AUTH_TOKEN) : '';
   if (!cachedToken || cachedToken.expiresOnTimestamp < Date.now()) {
-    let cachedCredential = new ManagedIdentityCredential();
+    let cachedCredential = new DefaultAzureCredential();
     cachedToken = await cachedCredential.getToken("https://cognitiveservices.azure.com/.default");
     process.env.AUTH_TOKEN = JSON.stringify(cachedToken);
     return cachedToken;
@@ -19,3 +16,4 @@ export async function getAuthToken() {
   // console.log("cachedToken from memory", cachedToken);
   return process.env.AUTH_TOKEN ? JSON.parse(process.env.AUTH_TOKEN) : '';;
 }
+ 
