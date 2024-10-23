90e9ce6541f92a60ba4cf96ca9cbb7f8af5193bf
Andy Chase <chase.andrew@epa.gov>
2024-10-23 21:37:38 +0000

    Fix environment variable based authentication

--- utils/lib/azure.js
+++ utils/lib/azure.js
@@ -1,11 +1,15 @@
 // lib/azure.js
 
 import { DefaultAzureCredential } from "@azure/identity";
+import { env } from "process";
 //import { getCache, setCache } from "./cache";
 
 
 
 export async function getAuthToken() {
+  if (env.AZURE_USE_MANAGED_IDENTITY=="false") {
+    return "";
+  }
   let cachedToken = process.env.AUTH_TOKEN ? JSON.parse(process.env.AUTH_TOKEN) : '';
   if (!cachedToken || cachedToken.expiresOnTimestamp < Date.now()) {
     let cachedCredential = new DefaultAzureCredential();
