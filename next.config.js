689106138fa916c7fe245649970f0e424a7733fe
Andy Chase <chase.andrew@epa.gov>
2024-10-28 23:38:02 +0000

    Remove CSP for google analytics

--- next.config.js
+++ next.config.js
@@ -1,17 +1,5 @@
 const { i18n } = require('./next-i18next.config');
 
-const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline';
-    style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data:;
-    font-src 'self';
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self';
-    frame-ancestors 'none';
-    upgrade-insecure-requests;
-`
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -32,10 +20,6 @@ const nextConfig = {
       {
         source: "/(.*)",
         headers: [
-          {
-            key: "Content-Security-Policy",
-            value: cspHeader.replace(/\n/g, ''),
-          },
           {
             key: "X-Content-Type-Options",
             value: "nosniff",
