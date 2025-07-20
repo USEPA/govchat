48cbb4fa4a7ffd5973708477aa1d32bf99596a0f
Andy Chase <theandychase@gmail.com>
2025-07-20 16:03:02 -0700

    Update vitest/Testing should read .env

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,9 +2,15 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  test: {
+    environment: 'node',
+    setupFiles: ['./__tests__/setup.ts'],
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './'),
     },
   },
+  envDir: './',
+  envPrefix: ['NEXT_PUBLIC_', 'OPENAI_', 'AZURE_', 'GOOGLE_', 'DEFAULT_'],
 });
