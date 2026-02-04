ad700e49dc802a8a3d408eefb9fa3d0f7ddcfa6f
Andy Chase <chase.andrew@epa.gov>
2026-02-04 14:04:21 -0800

    Adjust the webgrounding button
    On mac os x, it appears like a checkbox which isn't what we want. This patch fixes the styling

--- components/Buttons/EnableGroundingButting.tsx
+++ components/Buttons/EnableGroundingButting.tsx
@@ -7,13 +7,18 @@ interface Props {
 
 const EnableGroundingButton = ({ onToggle, enabled }: Props) => {
 	return (
-		<>
-			<input type="checkbox" id="enable-grounding" className="btn-check" checked={enabled} onClick={onToggle} />
-			<label className={`ml-1 mt-2 rounded-sm p-1 btn`} htmlFor="enable-grounding" title={enabled ? 'Disable Grounding' : 'Enable Grounding'}>
+		<span>
+			<button
+				onClick={onToggle}
+				className={`inline-block ml-1 mt-2 rounded-sm p-1 text-neutral-800 opacity-60 hover:bg-neutral-200 hover:text-neutral-900 dark:bg-opacity-50 dark:text-neutral-100 dark:hover:text-neutral-200 cursor-pointer ${
+					enabled ? 'bg-neutral-200 text-neutral-900 dark:bg-neutral-700 dark:text-neutral-200' : ''
+				}`}
+				title={enabled ? 'Disable Grounding' : 'Enable Grounding'}
+				aria-label={enabled ? 'Disable Grounding' : 'Enable Grounding'}
+			>
 				<IconWorldSearch size={18} />
-				<span className="sr-only">{enabled ? 'Disable Grounding' : 'Enable Grounding'}</span>
-			</label>
-		</>
+			</button>
+		</span>
 	)
 };
 
